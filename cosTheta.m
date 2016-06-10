6daada28cee3262f269086056f52c09851803ce1
geopete1 <adamsp@ufl.edu>
2016-06-10 09:47:03 -0400

    Sine improvement
    Added sine of theta calculation and plot

--- cosTheta.m
+++ cosTheta.m
@@ -2,9 +2,11 @@ close all,clear all
 
 theta = 0:2*pi/1000:2*pi;
 a = cos(theta);
+b = sin(theta);
 
-% Plot of cosine of an angle 'theta'
-plot(theta,a)
+% Plot of cosine and sine of an angle 'theta'
+plot(theta,a),hold on
+plot(theta,b,'r','linewidth',2)
 xlabel('\theta','FontName','Arial','FontSize',16)
-ylabel('Value of the cosine of \theta')
+ylabel('Value of the trig function of \theta')
 
