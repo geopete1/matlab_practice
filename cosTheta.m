a3c978a5186b2ef130f306dfad5b7cc3d1c847f1
J.F. Paniagua-Arroyave <jf.paniagua.ar@gmail.com>
2016-06-09 20:53:05 -0400

    Comment before plot
    This is a change proposed to the cosine-of-theta script...

--- cosTheta.m
+++ cosTheta.m
@@ -1,8 +1,9 @@
 close all,clear all
 
 theta = 0:2*pi/1000:2*pi;
-a = cos(theta)
+a = cos(theta);
 
+% Plot of cosine of an angle 'theta'
 plot(theta,a)
 xlabel('\theta','FontName','Arial','FontSize',16)
 ylabel('Value of the cosine of \theta')
