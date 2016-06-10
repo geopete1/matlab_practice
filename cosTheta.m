9e2ad234def36984eba0f2fdd56d63af41d6ab16
geopete1 <adamsp@ufl.edu>
2016-06-10 10:38:50 -0400

    plot improvements
    changed marker style for tangent, limited y axis, and added grid

--- cosTheta.m
+++ cosTheta.m
@@ -8,7 +8,8 @@ c = tan(theta);
 % Plot of cosine and sine of an angle 'theta'
 plot(theta,a),hold on
 plot(theta,b,'r','linewidth',2)
-plot(theta,c,'--g','linewidth',3)
+plot(theta,c,'g*','linewidth',3)
 xlabel('\theta','FontName','Arial','FontSize',16)
 ylabel('Value of the trig function of \theta')
-
+set(gca,'YLim',[-2 2])
+grid on
