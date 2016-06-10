6204c124b181246e321cd7ee9469588d1d8b6fc9
jfpa <jf.paniagua.ar@gmail.com>
2016-06-10 10:20:34 -0400

    Do not forget tangent!

---
+++ matlab_practice/cosTheta.m
@@ -0,0 +1,14 @@
+clc,close all,clear all
+
+theta = 0:2*pi/1000:2*pi;
+a = cos(theta);
+b = sin(theta);
+c = tan(theta);
+
+% Plot of cosine and sine of an angle 'theta'
+plot(theta,a),hold on
+plot(theta,b,'r','linewidth',2)
+plot(theta,c,'--g','linewidth',3)
+xlabel('\theta','FontName','Arial','FontSize',16)
+ylabel('Value of the trig function of \theta')
+
